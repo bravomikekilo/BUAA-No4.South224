61d951a0d4d08e9746c9304891f0bac15d9e3464
baomingkun <bmk1221@126.com>
2016-03-24 20:27:24 +0800

    phototype level but can work and not insane

--- simulation.cpp
+++ simulation.cpp
@@ -11,8 +11,8 @@ int main(void)
     //init
     double M0;
     cout<< "输入中心天体质量|> ";cin >> M0;
-    double M1;
-    cout<< "输入环绕天体质量|> ";cin >> M1;
+    //double M1;
+    //cout<< "输入环绕天体质量|> ";cin >> M1;
     double T ;
     cout<< "输入时间间隔|> ";cin >> T;
     COMPLEX X0;
@@ -21,20 +21,30 @@ int main(void)
     cout<< "输入第二基准点|> ";cin >> X1;
     double rows;
     cout<< "输入循环次数|> ";cin >> rows;
-    string filename;
-    cout<< "输入结果文件名|> ";cin >> filename;
-    fstream fs(filename.c_str());
+    //string filename;
+    //cout<< "输入结果文件名|> ";cin >> filename;
+    //fstream fs(filename.c_str());
     COMPLEX F = 0;
+    COMPLEX max = 0;
+    bool fall = 0;
+    double normal = T*T;
     for(int i = 0;i<rows;i++)
     {
-	F = G*M0*M1*f(X0);
-	cout<< "F |>"<<F<<endl;
+	//F = G*M0*M1*f(X0);
+	F = G*M0*f(X0);
+	//cout<< "F |>"<<F<<endl;
 	COMPLEX temp = X1;
-	X1 = (T*T/M1)+2.0*X1-X0;
-	cout<< "X |>"<<X1<<endl;
-	fs<<X0<<endl;
+	X1 = normal*F+2.0*X1-X0;
+	//cout<< "X |>"<<X1<<endl;
+	//fs<<X0<<endl;
 	X0 = temp;
+	if (abs(X1)>abs(max))
+	    max = X1;
+	else
+	    fall =1;
     }
+    cout<<max<<endl;
+    cout<<fall<<endl;
     return 0;
 }
 inline COMPLEX f(COMPLEX position)
